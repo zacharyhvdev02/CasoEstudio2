30e3aefa4c0e2e853cfd47a2a3808cb194ac9d82
Zachary Hernandez Valverde <zhernandezv@ucenfotec.ac.cr>
2024-07-21 21:33:03 -0600

    merged and fix bugs in merging

--- ilib.sql
+++ ilib.sql
@@ -1,124 +1,42 @@
--- phpMyAdmin SQL Dump
--- version 4.9.10
--- https://www.phpmyadmin.net/
---
--- Servidor: localhost
--- Tiempo de generación: 04-12-2022 a las 21:15:15
--- Versión del servidor: 5.7.21-log
--- Versión de PHP: 5.6.31
+CREATE DATABASE ilib;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+USE ilib;
 
+CREATE TABLE books (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255),
+    date VARCHAR(50),
+    author VARCHAR(255),
+    category VARCHAR(255),
+    edit VARCHAR(255),
+    lang VARCHAR(50),
+    pages VARCHAR(50),
+    description TEXT,
+    ejemplares VARCHAR(50),
+    stock INT,
+    available INT,
+    state INT,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    comments TEXT
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE lendings (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    book_id INT NOT NULL,
+    date_out VARCHAR(255) NOT NULL,
+    date_return VARCHAR(255) DEFAULT NULL
+)
 
---
--- Base de datos: `ilib`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `books`
---
-
-CREATE TABLE `books` (
-  `id` int(11) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `date` varchar(255) NOT NULL,
-  `author` varchar(255) NOT NULL,
-  `category` varchar(255) NOT NULL,
-  `edit` varchar(255) NOT NULL,
-  `lang` varchar(255) NOT NULL,
-  `pages` varchar(255) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  `ejemplares` varchar(255) NOT NULL,
-  `stock` int(11) NOT NULL,
-  `available` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `lendings`
---
-
-CREATE TABLE `lendings` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `book_id` int(11) NOT NULL,
-  `date_out` varchar(255) NOT NULL,
-  `date_return` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `name` varchar(30) NOT NULL,
-  `last_name_p` varchar(30) NOT NULL,
-  `last_name_m` varchar(30) NOT NULL,
-  `domicilio` varchar(250) DEFAULT NULL,
-  `tel` varchar(25) DEFAULT NULL,
-  `sanctions` int(11) DEFAULT '0',
-  `sanc_money` int(11) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `books`
---
-ALTER TABLE `books`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `lendings`
---
-ALTER TABLE `lendings`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `books`
---
-ALTER TABLE `books`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `lendings`
---
-ALTER TABLE `lendings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    last_name_p VARCHAR(30) NOT NULL,
+    last_name_m VARCHAR(30) NOT NULL,
+    domicilio VARCHAR(250) DEFAULT NULL,
+    tel VARCHAR(25) DEFAULT NULL,
+    sanctions INT DEFAULT '0',
+    sanc_money INT NOT NULL DEFAULT '0',
+    username VARCHAR(30) NOT NULL,
+    password VARCHAR(255) NOT NULL
+)
